85334c759b736d969631e6b6628150d083c4331f
Michael Kes <michaelkes@outlook.com>
2017-01-18 16:00:30 +0100

    Algemene profielpagina met formulier om gebruikersdata te wijzigen, beschikbaar vanuit de footer.

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,6 +5,7 @@
       if(basename($_SERVER['PHP_SELF']) == 'groep.php')
       { ?>
           <li><a href="groepen.php" class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Ga naar groepen"><i class="material-icons">home</i></a></li>
+          <li><a href="groepen.php">Home</a></li>
       <?php } ?>
     </ul>
   </div>
